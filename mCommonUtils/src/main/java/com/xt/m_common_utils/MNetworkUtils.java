016442356e1e853d89945423fcd5bcd618b4e293
alone <932583597@qq.com>
2018-09-29 14:29:06 +0800

    加入网络工具类

---
+++ mCommonUtils/src/main/java/com/xt/m_common_utils/MNetworkUtils.java
@@ -0,0 +1,12 @@
+package com.xt.m_common_utils;
+
+import com.blankj.utilcode.util.NetworkUtils;
+
+/**
+ * Created by xuti on 2018/9/29.
+ */
+public class MNetworkUtils {
+    public static boolean isConnected(){
+        return NetworkUtils.isConnected();
+    }
+}
