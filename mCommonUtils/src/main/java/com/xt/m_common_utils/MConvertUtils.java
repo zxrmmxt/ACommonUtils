fecb94242d93367dcb3fb2821f052d0da22cdd8d
alone <932583597@qq.com>
2018-09-29 13:51:33 +0800

    加入字节数组转换工具

---
+++ mCommonUtils/src/main/java/com/xt/m_common_utils/MConvertUtils.java
@@ -0,0 +1,12 @@
+package com.xt.m_common_utils;
+
+import com.blankj.utilcode.util.ConvertUtils;
+
+/**
+ * Created by xuti on 2018/9/29.
+ */
+public class MConvertUtils {
+    public static String bytes2HexString(byte[] bytes){
+        return ConvertUtils.bytes2HexString(bytes);
+    }
+}
