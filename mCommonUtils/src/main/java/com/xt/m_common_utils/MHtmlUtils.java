2b41bba3be682efef549beca35232e1f76789856
alone <932583597@qq.com>
2018-09-29 13:19:33 +0800

    加入html文件解析工具类

---
+++ mCommonUtils/src/main/java/com/xt/m_common_utils/MHtmlUtils.java
@@ -0,0 +1,41 @@
+package com.xt.m_common_utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * Created by xuti on 2018/5/22.
+ * 解析html
+ */
+
+public class MHtmlUtils {
+    public static void parseHtml() {
+        Document doc = null;
+        try {
+            String url = "";
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+                /*for (Element file : doc.select("a[mp4sTask$=.mp4]")) {
+                    LogUtils.d("getFileFromHtml------baseUri-------->" + file.baseUri());
+                    LogUtils.d("getFileFromHtml-------mp4sTask------->" + file.attr("mp4sTask"));
+                    LogUtils.d("getFileFromHtml--------text------>" + file.ownText());
+                }*/
+        if (doc == null) {
+            return;
+        }
+        String cssQuery = "a[href$=.jpg]";
+        cssQuery = "a[href$=.mp4]";
+        Elements elements = doc.select(cssQuery);
+        for (Element element : elements) {
+//            element.ownText()
+//            element.attr("href")
+//            element.baseUri()
+        }
+    }
+}
