80449836ef83b1095c5d18be97d6be9a0689d6e9
alone <932583597@qq.com>
2018-09-29 13:56:10 +0800

    aa

--- mCommonUtils/src/main/java/com/xt/m_common_utils/MConvertUtils.java
+++ mCommonUtils/src/main/java/com/xt/m_common_utils/MConvertUtils.java
@@ -6,7 +6,11 @@ import com.blankj.utilcode.util.ConvertUtils;
  * Created by xuti on 2018/9/29.
  */
 public class MConvertUtils {
-    public static String bytes2HexString(byte[] bytes){
+    public static String bytes2HexString(byte[] bytes) {
         return ConvertUtils.bytes2HexString(bytes);
+    }
+
+    public static byte[] hexString2Bytes(String hexString) {
+        return ConvertUtils.hexString2Bytes(hexString);
     }
 }
