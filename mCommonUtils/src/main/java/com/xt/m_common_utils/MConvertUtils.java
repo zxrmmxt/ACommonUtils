5af5e4e4a0f16986061d0925e44bf1eaeb7cc20f
alone <932583597@qq.com>
2018-10-12 17:25:14 +0800

    aa

--- mCommonUtils/src/main/java/com/xt/m_common_utils/MConvertUtils.java
+++ mCommonUtils/src/main/java/com/xt/m_common_utils/MConvertUtils.java
@@ -1,5 +1,7 @@
 package com.xt.m_common_utils;
 
+import android.support.annotation.NonNull;
+
 import com.blankj.utilcode.util.ConvertUtils;
 
 /**
@@ -7,10 +9,19 @@ import com.blankj.utilcode.util.ConvertUtils;
  */
 public class MConvertUtils {
     public static String bytes2HexString(byte[] bytes) {
+        return doBytes2HexString(bytes);
+    }
+
+    @NonNull
+    private static String doBytes2HexString(byte[] bytes) {
         return ConvertUtils.bytes2HexString(bytes);
     }
 
     public static byte[] hexString2Bytes(String hexString) {
+        return doHexString2Bytes(hexString);
+    }
+
+    private static byte[] doHexString2Bytes(String hexString) {
         return ConvertUtils.hexString2Bytes(hexString);
     }
 }
